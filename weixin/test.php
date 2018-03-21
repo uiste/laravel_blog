0d4b25b07f7fb81e9b0c9c5676893ed4a87b0e54
uiste <798407452.com>
2018-03-21 14:40:00 +0800

    微信test

---
+++ weixin/test.php
@@ -0,0 +1,70 @@
+<?php
+header("content-type:text/html;charset=utf8");
+$signature  = $_GET['signature'];
+$timestamp	= $_GET['timestamp'];
+$nonce 		= $_GET['nonce'];
+$echostr	= $_GET['echostr'];
+$token      = 'uiste';
+$array = array($nonce, $timestamp, $token);
+sort($array);
+//拼接成字符串,sha1加密 ，然后与signature进行校验
+$str = sha1( implode( $array ) );
+if( $str  == $signature && $echostr ){
+	//第一次接入weixin api接口的时候
+	echo  $echostr;
+	exit;
+}else{
+	reponseMsg();
+}
+
+function reponseMsg()
+{
+	// 接收事件推送并回复
+		//1.获取到微信推送过来post数据（xml格式）
+		$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+		//2.处理消息类型，并设置回复类型和内容
+		/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[FromUser]]></FromUserName>
+<CreateTime>123456789</CreateTime>
+<MsgType><![CDATA[event]]></MsgType>
+<Event><![CDATA[subscribe]]></Event>
+</xml>*/
+		$postObj = simplexml_load_string( $postArr );
+		//$postObj->ToUserName = '';
+		//$postObj->FromUserName = '';
+		//$postObj->CreateTime = '';
+		//$postObj->MsgType = '';
+		//$postObj->Event = '';
+		// gh_e79a177814ed
+		//判断该数据包是否是订阅的事件推送
+		if( strtolower( $postObj->MsgType) == 'event'){
+			//如果是关注 subscribe 事件
+			if( strtolower($postObj->Event == 'subscribe') ){
+				//回复用户消息(纯文本格式)	
+				$toUser   = $postObj->FromUserName;
+				$fromUser = $postObj->ToUserName;
+				$time     = time();
+				$msgType  =  'text';
+				$content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+				$template = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>";
+				$info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+				echo $info;
+/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[fromUser]]></FromUserName>
+<CreateTime>12345678</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[你好]]></Content>
+</xml>*/
+			
+
+			}
+		}
+}
